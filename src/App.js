220a95a9ae898ac15d527601011d09df55dbad7a
隆科(LongKe) <01398781@sf.com>
2020-10-14 15:34:25 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Fragment} from 'react'
+
+function App() {
+  return (
+    <Fragment>
+
+      <div className="App">
+                    <h1>This is React App.</h1>
+                </div>
+                <div className="App-other">
+                    <h1>This is React App-ohter.</h1>
+                </div>
+    </Fragment>
+  )
+}
+
+export default App
